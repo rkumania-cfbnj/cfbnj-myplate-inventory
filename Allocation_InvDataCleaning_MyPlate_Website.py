e7cabc5408993ef9cf133656805c1e7f5ccf89b0
rkumania-cfbnj <rkumania@cfbnj.org>
2026-09-21 13:01:23 -0400

    Update Allocation_InvDataCleaning_MyPlate_Website.py

--- Allocation_InvDataCleaning_MyPlate_Website.py
+++ Allocation_InvDataCleaning_MyPlate_Website.py
@@ -201,9 +201,9 @@ col_limits, col_targets = st.columns([2, 1])
 with col_limits:
     st.write("**💡 Important Note: Items expiring within 90 days are automatically added to the variety count. Your number below represents the additional varieties to select, not the final total.**")
     st.markdown(
-        "**Example:** You have **6 Cereal items**, and **1 is tagged as expiring within 90 days in excel file**. "
-        "SO if you enter **4** variety for Cereal, the final allocation will include **5 Cereal varieties** — "
-        "Because **4 selected + 1 automatically included expiring item.**"
+        "Example: You have 6 Cereal items and **1 is tagged as expiring within 90 days in excel file**. "
+        "SO if you enter **4** variety for Cereal, the final allocation will include **5 Cereal items** — "
+        "Because **4 selected + 1 automatically included expiring item from your excel.**"
     )
     for cat, default_val in default_limits.items():
         short_label = cat.split(':')[0].split('-')[0].strip()
